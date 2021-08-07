d8ef1c6592960eb325e41b219d9d73bfff9ce87e
minas528 <minalem.5d@gmail.com>
2021-08-07 12:32:17 +0300

    [utils] setup colors for global usage

---
+++ lib/utils/palettes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color blue = Color(0xff0056D2);
+Color lightBlue = Color(0xffadd8e6);
+Color red = Color(0xffEA6839);
+Color yellow = Color(0xffF9D013);
+Color green = Color(0xff069E80);
+Color columbiaBlue = Color(0xffB8d8d8);
+Color weldonBlue = Color(0xff7a9e9f);
+Color stormCloud = Color(0xff4f6367);
+Color beige = Color(0xffeef5db);
+Color sunsetOrange = Color(0xfffe5f55);
+Color dustStorm = Color(0xffddd1c7);
+Color silver = Color(0xffc2cfb2);
+Color darkSeaGreen = Color(0xff8db580);
+Color mummysTomb = Color(0xff7e8987);
+Color independence = Color(0xff4b4a67);
